7e317fe8e16f29e74d79b1c7aedd991af2d6cbad
Almir de Lima Barbosa <97858307+AlmirCbBarbosa@users.noreply.github.com>
2022-09-26 16:24:25 -0300

    Base
    Projeto com o mínimo de estruturas de arquivos e pastas criadas

---
+++ main.js
@@ -0,0 +1,6 @@
+//------ configurando Tela
+let canvas = document.getElementById("tela");
+let ctx = canvas.getContext("2d");
+
+canvas.width = 800; // largura do canvas
+canvas.height = 600; // altura do canvas
